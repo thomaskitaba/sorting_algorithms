e9bcaa88a6a70d70a7b5c312ab4731e78345b2ec
thomaskitaba <thomaskitaba@gmail.com>
2023-06-24 16:20:14 +0300

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,14 +1,19 @@
 #include "sort.h"
 void swap(int *x, int *y);
+void quick_sort_recursion(int *array, int low, int high);
+int partition(int *array, int low, int high);
+/**
+* swap - bubble sort
+* @x: x
+* @y: y
+*/
+void swap(int *x, int *y)
 {
     int *temp;
-    temp = *x;
+    temp = x;
     *x = *y;
     *y = *temp;
-
 }
-void quicksort_recursion(int *array, int low, int high);
-int partition(int *array, int low, int high);
 /**
 * quick_sort - bubble sort
 * @array: array
@@ -16,24 +21,16 @@ int partition(int *array, int low, int high);
 */
 void quick_sort(int *array, size_t size)
 {
-quicksort_recursion(array, 0, (int)size - 1);
-}
-/**
-* quicksort_recursion - bubble sort
-* @array: array
-* @size: size  of array
-*/
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+quick_sort_recursion(array, 0, (int)size - 1);
 }
 
+
 int partition(int arr[], int low, int high) {
+    int j;
     int pivot = arr[high];
     int i = low - 1;
 
-    for (int j = low; j < high; j++) {
+    for (j = low; j < high; j++) {
         if (arr[j] <= pivot) {
             i++;
             swap(&arr[i], &arr[j]);
@@ -44,11 +41,11 @@ int partition(int arr[], int low, int high) {
     return i + 1;
 }
 
-void quick_sort_recursive(int arr[], int low, int high) {
+void quick_sort_recursion(int arr[], int low, int high) {
     if (low < high) {
         int pivot_index = partition(arr, low, high);
 
-        quick_sort_recursive(arr, low, pivot_index - 1);
-        quick_sort_recursive(arr, pivot_index + 1, high);
+        quick_sort_recursion(arr, low, pivot_index - 1);
+        quick_sort_recursion(arr, pivot_index + 1, high);
     }
 }
